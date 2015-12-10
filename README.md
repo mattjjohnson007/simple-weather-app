3f57594fde3447508da14146e433b0d08d1b2c01
mattjjohnson007 <mattjjohnson007@gmail.com>
2015-12-09 18:24:35 -0600

    yay.

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ Simple Weather Application
 This application uses the [Open Weather API](http://openweathermap.org/api) to display current weather data for a city.
 
 To use this application you must have a key for the API that you will enter when prompted.  Once entered, this key will be stored in your browser for later use.
+
+36789c5dc063c2a75bfd70722a3641ed
--- js/script.js
+++ js/script.js
@@ -1,6 +1,110 @@
-$.ready(function() {
+$(document).ready(function () {
+
+    console.log("hello");
     
-   console.log("hello"); 
+//    console.log(weatherDesc);
+
+    weatherApp = {
+        $targetArea: $("#weather"),
+        weatherApiKey: "",
+        localStorageKey: "openWeatherApi",
+        //clearDataLocally: "clear",
+
+        getFormData: function () {
+            if (weatherApp.weatherApiKey === null || weatherApp.weatherApiKey === "") {
+                weatherApp.weatherApiKey = $("#apikey").val();
+                weatherApp.weatherApiKey = weatherApp.weatherApiKey.trim();
+            weatherApp.saveApiKey();
+            }
+            
+            var zip = $("#zip").val().trim();
+            if (zip === null || zip.length < 5 ) {
+                weatherApp.$targetArea.html("Enter a valid zip code.");
+            } else {
+                weatherApp.getWeatherData(zip);
+            }
+
+            console.log(apikey);
+            console.log(zip);
+        },
+        
+        getWeatherData : function (zipcode)
+         {
+           var url = "http://api.openweathermap.org/data/2.5/weather?zip=" + zipcode + ",us&appid=" + weatherApp.weatherApiKey + "&units=imperial";  
+             $.getJSON(url,function ( data ) {
+                 
+                 if (data.cod === 200) {
+//                     weatherApp.$targetArea.html("Success!");
+                     
+                      $("#tempDisp").append("Temperature");
+                    $("#tempValue").append(Math.round(data.main.temp));
+                    $("#lblDes").append(data.weather[0].description);
+                     
+//                       This is where you would add data to the page
+                     
+//                     weatherDesc = data.weather[0].description;
+//                     console.log(weatherDesc);                     
+                     
+                 } else {
+                 weatherApp.$targetArea.html("Sorry, no weather data available. Try again later.");
+                 } 
+             }).fail (function(){
+                 weatherApp.$targetArea.html("Sorry, no weather data available.  Try again later.");
+            
+                      
+         }) ; 
+         },
+
+        loadApiKey: function () {
+            if (typeof (localStorage) === 'undefined') {
+                weatherApp.$targetArea.html = ("Sorry, local storage is not supported for this browser.");
+            } else {
+                // Get API Key
+                weatherApp.weatherApiKey = localStorage.getItem(weatherApp.localStorageKey);
+                if (weatherApp.weatherApiKey === null || weatherApp.weatherApiKey === "") {
+                    //weatherApp.$targetArea.html("Sorry, no api key was found.");
+                    return false;
+                }
+                return true;
+            }
+        },
+
+        saveApiKey: function () {
+            if (typeof (localStorage) === 'undefined') {
+                weatherApp.$targetArea.html("Sorry");
+            } else {
+                if (weatherApp.weatherApiKey === null || weatherApp.weatherApiKey === "") {
+                    weatherApp.$targetArea.html("Sorry");
+                } else {
+                    localStorage.setItem(weatherApp.localStorageKey, weatherApp.weatherApiKey);
+                    $("#apidiv").attr("class", "hide");
+                }
+            }
+        },
+           clearDataLocally: function (){
+        if (typeof(localStorage) === 'undefined') {
+            targetArea.html = "Sorry, local storage is not supported for this browser.";
+        }
+        else {
+            localStorage.removeItem(localStorageKey);
+        }
+    }
+    }
+    
+//    var jsStr = JSON.stringify(weatherApp);
+//    
+//    document.body.HTML = jsStr;
     
     
+    $("#submit").click(function () {
+
+        weatherApp.getFormData();
+        return false;
+
+    });
+
+    if (weatherApp.loadApiKey()) {
+        $("#apidiv").attr("class", "hide");
+        
+    }
 });
